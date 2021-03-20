8d3bd866f0b0963444c09c6b94a2c2c87bff7b7c
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-20 16:26:10 -0700

    set up GQL server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,46 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import 'reflect-metadata';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 
-createConnection().then(async connection => {
+// server set up
+(async () => {
+  const app = express();
+  app.get('/', (_req, res) => res.send('Hello World'));
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+  const apolloServer = new ApolloServer({
+    typeDefs: `
+      type Query{
+          hello: String!
+      }
+      `,
+    resolvers: {
+      Query: {
+        hello: () => 'heLLo WuRdL'
+      }
+    }
+  });
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+  apolloServer.applyMiddleware({ app });
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+  app.listen(4000, () => {
+    console.log('express server started');
+  });
+})();
 
-}).catch(error => console.log(error));
+// createConnection()
+//   .then(async (connection) => {
+//     console.log('Inserting a new user into the database...');
+//     const user = new User();
+//     user.firstName = 'Timber';
+//     user.lastName = 'Saw';
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log('Saved a new user with id: ' + user.id);
+
+//     console.log('Loading users from the database...');
+//     const users = await connection.manager.find(User);
+//     console.log('Loaded users: ', users);
+
+//     console.log('Here you can setup and run express/koa/any other framework.');
+//   })
+//   .catch((error) => console.log(error));
