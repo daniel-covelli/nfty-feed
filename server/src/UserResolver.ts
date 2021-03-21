3b56f7a0aa22a147b270751c550e88d1063b0ba0
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-20 18:48:54 -0700

    add login method

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,19 @@
-import { Resolver, Query, Arg, Mutation } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Arg,
+  Mutation,
+  ObjectType,
+  Field
+} from 'type-graphql';
 import { User } from './entity/User';
-import { hash } from 'bcryptjs';
+import { hash, compare } from 'bcryptjs';
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -12,6 +25,26 @@ export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('could not find user');
+    }
+
+    const valid = compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('nvalid password');
+    }
+
+    return { accessToken: '' };
   }
 
   @Mutation(() => Boolean)
