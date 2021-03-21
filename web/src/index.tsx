9cfa2cd2af8b23e57019aa89781a71fa8ad342ad
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-21 14:07:13 -0700

    wrap project in apollo provider

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
+
+ReactDOM.render(
+  <ApolloProvider>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
