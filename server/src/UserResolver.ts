33d0bdeb4eea0c8ee589639dea1e1db6b5bb522d
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-20 18:39:13 -0700

    add query for all users

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -9,6 +9,11 @@ export class UserResolver {
     return 'hi!';
   }
 
+  @Query(() => [User])
+  users() {
+    return User.find();
+  }
+
   @Mutation(() => Boolean)
   async register(
     @Arg('email') email: string,
--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,10 +1,14 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
+import { ObjectType, Field } from 'type-graphql';
 
+@ObjectType()
 @Entity('users')
 export class User extends BaseEntity {
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   email: string;
 
