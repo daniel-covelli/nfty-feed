8c87d5540a3e8fa8b7fd30907e9a0f36425ae2dc
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-21 14:21:12 -0700

    graphql useQuery  sanity check

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 const App: React.FC = () => {
-  return <div>NOTHING YET</div>;
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      NOTHING YET
+      <p>{JSON.stringify(data)}</p>
+    </div>
+  );
 };
 
 export default App;
