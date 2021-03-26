4bcbbd3c8a30d0d123819fe214050e7d4c8b5d21
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-26 14:37:37 -0700

    add types for react-router-dom

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import App from './App';
+import Routes from './Routes';
 
 const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Routes />
   </ApolloProvider>,
   document.getElementById('root')
 );
