0a22e8dfb35d4227b8d28361ad3d2f1c4458d66b
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-26 15:29:02 -0700

    add register mutation as hook in register.tsx

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
+import { useRegisterMutation } from '../generated/graphql';
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = ({}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [register] = useRegisterMutation();
+
   return (
     <form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log('form submitted');
         console.log(email, password);
+        const response = await register({ variables: { email, password } });
       }}>
       <div>
         <input
