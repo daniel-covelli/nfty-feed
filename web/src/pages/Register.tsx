4114c52ab4deb186be04acd90f492dc6fd77ca8f
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-26 15:11:05 -0700

    start user registration form

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = ({}) => {
-  return <div>register page</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <form>
+      <div>
+        <input
+          value={email}
+          placeholder='email'
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          value={password}
+          placeholder='password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+    </form>
+  );
 };
