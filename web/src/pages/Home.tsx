1d2b55bc160ec5d8ded418c418652e31822af65e
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-26 15:42:27 -0700

    add useUsersQuery using network request to homepage

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useUsersQuery } from '../generated/graphql';
 
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = ({}) => {
+  const {} = useUsersQuery({ fetchPolicy: 'network-only' });
   return <div>home page</div>;
 };
