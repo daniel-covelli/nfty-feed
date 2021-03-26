c62f1a1fc96fa1af24441c5e35eec85dc8f17e8d
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-26 14:57:00 -0700

    added three pages as routes in Routes.tsx

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/' render={() => <div>hi</div>} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </BrowserRouter>
   );
