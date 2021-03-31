295656cca5d5aecdaec890f5957ff1fc6b6569c3
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-30 22:02:22 -0700

    create me query which uses the context to find and return the current user

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -16,6 +16,7 @@ import { createAccessToken, createRefreshToken } from './auth';
 import { isAuth } from './isAuth';
 import { sendRefreshToken } from './sendRefreshToken';
 import { getConnection } from 'typeorm';
+import { verify } from 'jsonwebtoken';
 
 @ObjectType()
 class LoginResponse {
@@ -33,13 +34,30 @@ export class UserResolver {
   @Query(() => String)
   @UseMiddleware(isAuth)
   bye(@Ctx() { payload }: MyContext) {
-    console.log(payload);
     return `your user id is: ${payload!.userId}`;
   }
 
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Query(() => User, { nullable: true })
+  me(@Ctx() context: MyContext) {
+    const authorization = context.req.headers['authorization'];
+
+    if (!authorization) {
+      return null;
+    }
+
+    try {
+      const token = authorization.split(' ')[1];
+      const payload: any = verify(token, process.env.ACCESS_TOKEN_SECRET!);
+      return User.findOne(payload.userId);
+    } catch (err) {
+      console.log(err);
+      return null;
+    }
   }
 
   @Mutation(() => LoginResponse)
