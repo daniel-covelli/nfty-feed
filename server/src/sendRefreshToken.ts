6bbd4df4795cd75514819cd469d381fd69c27a37
Daniel Covelli <danielcovelli@berkeley.edu>
2021-03-30 23:35:28 -0700

    add logout functionality and restrict token to refresh_token path

--- server/src/sendRefreshToken.ts
+++ server/src/sendRefreshToken.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie('jid', token, { httpOnly: true });
+  res.cookie('jid', token, { httpOnly: true, path: '/refresh_token' });
 };
--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useMeQuery } from './generated/graphql';
+import { useMeQuery, useLogoutMutation } from './generated/graphql';
+import { setAccessToken } from './accessToken';
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   const { data, loading } = useMeQuery();
+  const [logout, { client }] = useLogoutMutation();
 
   let body: any = null;
 
@@ -29,6 +31,18 @@ export const Header: React.FC<HeaderProps> = ({}) => {
       </div>
       <div>
         <Link to='/bye'>bye</Link>
+      </div>
+      <div>
+        {!loading && data && data.me ? (
+          <button
+            onClick={async () => {
+              await logout();
+              setAccessToken('');
+              await client!.resetStore();
+            }}>
+            logout
+          </button>
+        ) : null}
       </div>
       {body}
     </header>
