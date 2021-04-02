a2f38a954a932489fe917d868aad94cd90ac9300
Daniel Covelli <danielcovelli@berkeley.edu>
2021-04-01 19:13:17 -0700

    edited cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ import { sendRefreshToken } from './sendRefreshToken';
   const app = express();
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: `http://localhost:${process.env.PORT || 3000}`,
       credentials: true
     })
   );
