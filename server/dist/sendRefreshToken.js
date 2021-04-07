70dfeb95e2bad8880aef5d5902b6b542de06567c
Daniel Covelli <danielcovelli@berkeley.edu>
2021-04-07 10:43:59 -0700

    gitignore file now working

--- server/dist/sendRefreshToken.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendRefreshToken = void 0;
-const sendRefreshToken = (res, token) => {
-    res.cookie('jid', token, { httpOnly: true, path: '/refresh_token' });
-};
-exports.sendRefreshToken = sendRefreshToken;
-//# sourceMappingURL=sendRefreshToken.js.map
