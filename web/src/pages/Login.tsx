d9fb78c4de37824c76800391cf4bec75dd0347a7
Daniel Covelli <danielcovelli@berkeley.edu>
2021-04-15 17:55:35 -0700

    front end profile form

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -16,7 +16,8 @@ import {
   Center,
   Box,
   Text,
-  Link
+  Link,
+  SimpleGrid
 } from '@chakra-ui/react';
 import { Formik, Form, Field } from 'formik';
 
@@ -30,97 +31,106 @@ export const Login: React.FC<RouteComponentProps> = ({ history }) => {
   }
 
   return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      onSubmit={async (values) => {
-        const email = values.email;
-        const password = values.password;
-        const response = await login({
-          variables: { email, password },
-          update: (store, { data }) => {
-            if (!data) {
-              return null;
-            }
-            store.writeQuery<MeQuery>({
-              query: MeDocument,
-              data: {
-                __typename: 'Query',
-                me: data.login.user
-              }
-            });
-          }
-        });
-        if (!response.data) {
-          toast({
-            title: `Invalid credentials! Please try again...`,
-            status: 'error',
-            position: 'top',
-            variant: 'subtle',
-            isClosable: true
-          });
-        }
-        if (response && response.data) {
-          setAccessToken(response.data.login.accessToken);
-          history.push('/');
-          toast({
-            title: `Welcome back, ‎you're logged in!!`,
-            status: 'success',
-            position: 'bottom',
-            variant: 'subtle',
-            isClosable: true
-          });
-        }
-      }}>
-      <Center>
-        <Form>
-          <Box pb='10px'>
-            <Field id='email' name='email'>
-              {({ field }) => (
-                <FormControl>
-                  <Input {...field} id='email' placeholder='email' />
-                </FormControl>
-              )}
-            </Field>
-          </Box>
-          <Box pb='10px'>
-            <Field id='password' name='password'>
-              {({ field }) => (
-                <FormControl>
-                  <Input
-                    w='250px'
-                    {...field}
-                    id='password'
-                    type='password'
-                    placeholder='password'
-                  />
-                </FormControl>
-              )}
-            </Field>
-          </Box>
-          <Box pb='10px'>
-            <Button
-              type='submit'
-              colorScheme='pink'
-              variant='outline'
-              size='sm'>
-              login
-            </Button>
-          </Box>
-
-          <Text fontSize='sm'>
-            Don't have an account?{' '}
-            <Link
-              as={ReactLink}
-              to='/register'
-              color='teal.500'
-              _focus={{
-                boxShadow: 'none'
-              }}>
-              register
-            </Link>
+    <Center>
+      <SimpleGrid columns={1} spacing='10px'>
+        <Box>
+          <Text fontSize='lg'>
+            <b>Login</b>
           </Text>
-        </Form>
-      </Center>
-    </Formik>
+        </Box>
+        <Box>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={async (values) => {
+              const email = values.email;
+              const password = values.password;
+              const response = await login({
+                variables: { email, password },
+                update: (store, { data }) => {
+                  if (!data) {
+                    return null;
+                  }
+                  store.writeQuery<MeQuery>({
+                    query: MeDocument,
+                    data: {
+                      __typename: 'Query',
+                      me: data.login.user
+                    }
+                  });
+                }
+              });
+              if (!response.data) {
+                toast({
+                  title: `Invalid credentials! Please try again...`,
+                  status: 'error',
+                  position: 'top',
+                  variant: 'subtle',
+                  isClosable: true
+                });
+              }
+              if (response && response.data) {
+                setAccessToken(response.data.login.accessToken);
+                history.push('/');
+                toast({
+                  title: `Welcome back, ‎you're logged in!!`,
+                  status: 'success',
+                  position: 'bottom',
+                  variant: 'subtle',
+                  isClosable: true
+                });
+              }
+            }}>
+            <Form>
+              <Box pb='10px'>
+                <Field id='email' name='email'>
+                  {({ field }) => (
+                    <FormControl>
+                      <Input {...field} id='email' placeholder='email' />
+                    </FormControl>
+                  )}
+                </Field>
+              </Box>
+              <Box pb='10px'>
+                <Field id='password' name='password'>
+                  {({ field }) => (
+                    <FormControl>
+                      <Input
+                        w='250px'
+                        {...field}
+                        id='password'
+                        type='password'
+                        placeholder='password'
+                      />
+                    </FormControl>
+                  )}
+                </Field>
+              </Box>
+              <Box pb='10px'>
+                <Button
+                  type='submit'
+                  colorScheme='pink'
+                  variant='outline'
+                  size='sm'>
+                  login
+                </Button>
+              </Box>
+
+              <Text fontSize='sm'>
+                Don't have an account?{' '}
+                <Link
+                  as={ReactLink}
+                  to='/register'
+                  color='teal.500'
+                  _focus={{
+                    boxShadow: 'none'
+                  }}>
+                  register
+                </Link>
+              </Text>
+            </Form>
+          </Formik>
+        </Box>
+      </SimpleGrid>
+    </Center>
   );
 };
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -18,10 +18,15 @@ import {
   Button,
   useToast,
   Text,
-  Link
+  Link,
+  SimpleGrid,
+  SlideFade,
+  useDisclosure,
+  Textarea
 } from '@chakra-ui/react';
 
 export const Register: React.FC<RouteComponentProps> = ({ history }) => {
+  const { isOpen, onToggle } = useDisclosure();
   const { data } = useMeQuery();
 
   if (data && data.me) {
@@ -33,98 +38,251 @@ export const Register: React.FC<RouteComponentProps> = ({ history }) => {
   const toast = useToast();
 
   return (
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      onSubmit={async (values) => {
-        const email = values.email;
-        const password = values.password;
+    <Center>
+      <SimpleGrid columns={1} spacing='10px'>
+        {isOpen ? (
+          <SlideFade in={isOpen}>
+            <Box mb='10px'>
+              <Text fontSize='lg'>
+                <b>Profile</b>
+              </Text>
+            </Box>
+            <Box>
+              <Formik
+                initialValues={{ username: '', first: '', last: '', bio: '' }}
+                onSubmit={async (values) => {
+                  const email = values.first;
+                  const password = values.last;
 
-        const { data } = await register({
-          variables: { email, password }
-        });
-        if (!data.register.res) {
-          toast({
-            title: data.register.message,
-            status: 'error',
-            position: 'top',
-            variant: 'subtle',
-            isClosable: true
-          });
-        } else {
-          await login({
-            variables: { email, password },
-            update: (store, { data }) => {
-              if (!data) {
-                return null;
-              }
-              store.writeQuery<MeQuery>({
-                query: MeDocument,
-                data: {
-                  __typename: 'Query',
-                  me: data.login.user
-                }
-              });
-            }
-          });
-          history.push('/');
-          toast({
-            title: `Congradulations 🎉‏‏‎‏‏‎ ‎‏‏‎ ‎ ‎you are registered!!`,
-            status: 'success',
-            position: 'bottom',
-            variant: 'subtle',
-            isClosable: true
-          });
-        }
-      }}>
-      <Center>
-        <Form>
-          <Box pb='10px'>
-            <Field id='email' name='email'>
-              {({ field }) => (
-                <FormControl>
-                  <Input {...field} id='email' placeholder='email' />
-                </FormControl>
-              )}
-            </Field>
-          </Box>
-          <Box pb='10px'>
-            <Field id='password' name='password'>
-              {({ field }) => (
-                <FormControl>
-                  <Input
-                    w='250px'
-                    {...field}
-                    id='password'
-                    placeholder='password'
-                    type='password'
-                  />
-                </FormControl>
-              )}
-            </Field>
-          </Box>
-          <Box pb='10px'>
-            <Button
-              type='submit'
-              colorScheme='pink'
-              variant='outline'
-              size='sm'>
-              register
-            </Button>
-          </Box>
-          <Text fontSize='sm'>
-            Already have an account?{' '}
-            <Link
-              as={ReactLink}
-              to='/login'
-              color='teal.500'
-              _focus={{
-                boxShadow: 'none'
-              }}>
-              login
-            </Link>
-          </Text>
-        </Form>
-      </Center>
-    </Formik>
+                  // const { data } = await register({
+                  //   variables: { email, password }
+                  // });
+                  // if (!data.register.res) {
+                  //   toast({
+                  //     title: data.register.message,
+                  //     status: 'error',
+                  //     position: 'top',
+                  //     variant: 'subtle',
+                  //     isClosable: true
+                  //   });
+                  // } else {
+                  //   await login({
+                  //     variables: { email, password },
+                  //     update: (store, { data }) => {
+                  //       if (!data) {
+                  //         return null;
+                  //       }
+                  //       store.writeQuery<MeQuery>({
+                  //         query: MeDocument,
+                  //         data: {
+                  //           __typename: 'Query',
+                  //           me: data.login.user
+                  //         }
+                  //       });
+                  //     }
+                  //   });
+                  // onToggle();
+                  // toast({
+                  //   title: `Congradulations 🎉‏‏‎‏‏‎ ‎‏‏‎ ‎ ‎you are registered!!`,
+                  //   status: 'success',
+                  //   position: 'bottom',
+                  //   variant: 'subtle',
+                  //   isClosable: true
+                  // });
+                }}>
+                <Form>
+                  <Box pb='10px'>
+                    <Field id='image' name='image'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Text fontSize='xs'>Image</Text>
+                          <input
+                            accept='image/*'
+                            multiple='false'
+                            id='image'
+                            placeholder='image'
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                    <div
+                      style={{
+                        height: '60px',
+                        width: '60px',
+                        border: '2px dashed black'
+                      }}>
+                      <img
+                        style={{
+                          width: '100%',
+                          height: '100%'
+                        }}
+                      />
+                    </div>
+                  </Box>
+                  <Box pb='10px'>
+                    <Field id='username' name='username'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Text fontSize='xs'>Username</Text>
+                          <Input
+                            {...field}
+                            id='username'
+                            placeholder='username'
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Box>
+                  <Box pb='10px'>
+                    <Field id='first' name='first'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Text fontSize='xs'>First</Text>
+                          <Input {...field} id='first' placeholder='first' />
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Box>
+                  <Box pb='10px'>
+                    <Field id='last' name='last'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Text fontSize='xs'>Last</Text>
+                          <Input
+                            w='250px'
+                            {...field}
+                            id='last'
+                            placeholder='last'
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Box>
+                  <Box pb='10px'>
+                    <Field id='bio' name='bio'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Text fontSize='xs'>Bio</Text>
+                          <Textarea {...field} id='bio' placeholder='bio' />
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Box>
+                  <Box pb='10px'>
+                    <Button
+                      type='submit'
+                      colorScheme='pink'
+                      variant='outline'
+                      size='sm'>
+                      continue
+                    </Button>
+                  </Box>
+                </Form>
+              </Formik>
+            </Box>
+          </SlideFade>
+        ) : (
+          <>
+            <Box>
+              <Text fontSize='lg'>
+                <b>Register</b>
+              </Text>
+            </Box>
+            <Box>
+              <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={async (values) => {
+                  const email = values.email;
+                  const password = values.password;
+
+                  // const { data } = await register({
+                  //   variables: { email, password }
+                  // });
+                  // if (!data.register.res) {
+                  //   toast({
+                  //     title: data.register.message,
+                  //     status: 'error',
+                  //     position: 'top',
+                  //     variant: 'subtle',
+                  //     isClosable: true
+                  //   });
+                  // } else {
+                  //   await login({
+                  //     variables: { email, password },
+                  //     update: (store, { data }) => {
+                  //       if (!data) {
+                  //         return null;
+                  //       }
+                  //       store.writeQuery<MeQuery>({
+                  //         query: MeDocument,
+                  //         data: {
+                  //           __typename: 'Query',
+                  //           me: data.login.user
+                  //         }
+                  //       });
+                  //     }
+                  //   });
+                  onToggle();
+                  toast({
+                    title: `Congradulations 🎉‏‏‎‏‏‎ ‎‏‏‎ ‎ ‎you are registered!!`,
+                    status: 'success',
+                    position: 'bottom',
+                    variant: 'subtle',
+                    isClosable: true
+                  });
+                }}>
+                <Form>
+                  <Box pb='10px'>
+                    <Field id='email' name='email'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Input {...field} id='email' placeholder='email' />
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Box>
+                  <Box pb='10px'>
+                    <Field id='password' name='password'>
+                      {({ field }) => (
+                        <FormControl>
+                          <Input
+                            w='250px'
+                            {...field}
+                            id='password'
+                            placeholder='password'
+                            type='password'
+                          />
+                        </FormControl>
+                      )}
+                    </Field>
+                  </Box>
+                  <Box pb='10px'>
+                    <Button
+                      type='submit'
+                      colorScheme='pink'
+                      variant='outline'
+                      size='sm'>
+                      register
+                    </Button>
+                  </Box>
+                  <Text fontSize='sm'>
+                    Already have an account?{' '}
+                    <Link
+                      as={ReactLink}
+                      to='/login'
+                      color='teal.500'
+                      _focus={{
+                        boxShadow: 'none'
+                      }}>
+                      login
+                    </Link>
+                  </Text>
+                </Form>
+              </Formik>
+            </Box>
+          </>
+        )}
+      </SimpleGrid>
+    </Center>
   );
 };
